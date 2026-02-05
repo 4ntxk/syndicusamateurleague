f0b35c872cb8ed842f9843b2025cd1e8cb3e85ad
Antek <antonstanaszek@gmail.com>
2026-02-05 22:28:10 +0100

    standings fix3

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,11 @@
 import type { Metadata } from 'next'
+import { Geist, Geist_Mono } from 'next/font/google'
 import { cookies, headers } from 'next/headers'
 import '../styles/globals.css'
 import { ClientLayout } from './ClientLayout'
 
+const geist = Geist({ subsets: ['latin'] })
+const geistMono = Geist_Mono({ subsets: ['latin'] })
 const supportedLocales = new Set(['pl', 'en'])
 
 export const metadata: Metadata = {
@@ -24,7 +27,7 @@ export default async function RootLayout({
 
   return (
     <html lang={locale}>
-      <body className={`font-sans antialiased`}>
+      <body className={`${geist.className} ${geistMono.className} font-sans antialiased`}>
         <ClientLayout>{children}</ClientLayout>
       </body>
     </html>
